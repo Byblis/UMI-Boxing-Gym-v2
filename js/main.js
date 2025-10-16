c1a3a655ba819ae8a403cbfb9b62138e6e861684
Byblis <karumoh@gmail.com>
2025-10-16 13:02:31 +0900

    スマホ見た目調整

--- js/main.js
+++ js/main.js
@@ -18,3 +18,10 @@ const nav = document.querySelector('nav');
 menuBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
